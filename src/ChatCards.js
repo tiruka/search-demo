ffb2642271017e5dafe42f23b90702368e05dea8
iruka t <tiruka.engineer@gmail.com>
2023-04-22 17:46:18 +0900

    add

--- src/ChatCards.js
+++ src/ChatCards.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from "react";
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Card from '@mui/material/Card';
@@ -8,17 +8,39 @@ import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 
+
+const Typewriter = ({ text }) => {
+  const [displayText, setDisplayText] = useState("");
+
+  useEffect(() => {
+    let currentIndex = 0;
+    const interval = setInterval(() => {
+      if (currentIndex === text.length - 1) {
+        clearInterval(interval);
+        return;
+      }
+      setDisplayText((prev) => prev + text[currentIndex]);
+      currentIndex++;
+    }, 30);
+    return () => clearInterval(interval);
+  }, [text]);
+
+  return <span>{displayText}</span>;
+};
+
 export default function ChatCards() {
+  const text = `旅行に関する情報が欲しい場合は、ウェブページが最適な情報源になります。
+  安くて快適なホテルを見つけるためには、比較サイトや口コミサイトが役立ちます。
+  また、旅行前に必要なアイテムや持ち物、パッキングのコツ、旅行保険や現地通貨の手配に関する情報が欲しい場合は、
+  旅行ガイドブックや専門サイトを利用することをおすすめします。京都に関する情報を得たい場合は、
+  観光地の公式サイトや観光案内所などが役立ちます。`
+
   return (
     <Grid item xs={12} sm={6} key={1}>
       <Card sx={{ backgroundColor: "#F2F2F2" }}>
         <CardContent>
           <Typography variant="body1">
-          旅行に関する情報が欲しい場合は、ウェブページが最適な情報源になります。
-          安くて快適なホテルを見つけるためには、比較サイトや口コミサイトが役立ちます。
-          また、旅行前に必要なアイテムや持ち物、パッキングのコツ、旅行保険や現地通貨の手配に関する情報が欲しい場合は、
-          旅行ガイドブックや専門サイトを利用することをおすすめします。京都に関する情報を得たい場合は、
-          観光地の公式サイトや観光案内所などが役立ちます。
+            <Typewriter text={text}/>
           </Typography>
         </CardContent>
       </Card>
