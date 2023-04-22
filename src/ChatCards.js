1b165d2ac00b6a0bd6722584f704b2a74572697a
iruka t <tiruka.engineer@gmail.com>
2023-04-22 17:29:42 +0900

    add

--- src/ChatCards.js
+++ src/ChatCards.js
@@ -13,7 +13,13 @@ export default function ChatCards() {
     <Grid item xs={12} sm={6} key={1}>
       <Card sx={{ backgroundColor: "#F2F2F2" }}>
         <CardContent>
-          <Typography variant="h5">ちゃっとの結果をchatGPTから持ってくる</Typography>
+          <Typography variant="body1">
+          旅行に関する情報が欲しい場合は、ウェブページが最適な情報源になります。
+          安くて快適なホテルを見つけるためには、比較サイトや口コミサイトが役立ちます。
+          また、旅行前に必要なアイテムや持ち物、パッキングのコツ、旅行保険や現地通貨の手配に関する情報が欲しい場合は、
+          旅行ガイドブックや専門サイトを利用することをおすすめします。京都に関する情報を得たい場合は、
+          観光地の公式サイトや観光案内所などが役立ちます。
+          </Typography>
         </CardContent>
       </Card>
       <TextField
