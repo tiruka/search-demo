789acaec40b7b16a4211bc0154cf4e1f95ada552
iruka t <tiruka.engineer@gmail.com>
2023-04-22 23:12:17 +0900

    add

--- src/ResultCards.js
+++ src/ResultCards.js
@@ -12,20 +12,19 @@ import TextField from '@mui/material/TextField';
 
 export default function ResultCards() {
   const cardData = [
-    { title: '箱根についてのご提案結果' },
-    { title: '京都についてのご提案結果' },
-    { title: 'Yサービスからのご提案'},
+    { title: '箱根についてのご提案' },
+    { title: '京都についてのご提案' },
+    { title: '類似のご提案【金沢】'},
     { title: '箱根市のおすすめホテル・旅館 20選', content: '湯河原・強羅・箱根湯本 · 京王プレリアホテル. 4.6. (733件) · ハートンホテル 4.4 · ダイワロイネットホテル. 4.3 · ホテルオークラ. 4.8.' },
     { title: '京都のおすすめ観光サイトTop10', content: '京都を代表する観光名所や京都の見どころまとめ、京都旅の中でしか出会えない京都の魅力をお楽しみください。金閣寺（鹿苑寺）...' },
-    { title: 'Y旅行サイトで予約', content: '【Y.comは即時値引きでお得】さらにタイムセールや期間限定プランなどが満載。 【施設情報】【伝統と革新の融合】' },
+    { title: '金沢のおすすめ観光サイトTop10', content: '【Y.comは即時値引きでお得】さらにタイムセールや期間限定プランなどが満載。 【施設情報】【伝統と革新の融合】' },
     { title: '箱根のおすすめ観光サイトのプラン', content: '旅の大切な思い出となるような贅沢で楽しいひとときを過ごせる有名スポットのご提案...' },
     { title: '京都旅行・京都ツアー おすすめプランを紹介', content: '宇治、天橋立などの情報も満載。国内旅行・国内ツアーのご予約・お問合せなAAA交通社.' },
-    { title: 'Y Shoppingで旅行の準備', content: '旅行に必要な日用品やスーツケースの検索結果を表示します。クーポン情報もお探しいたします。' },
+    { title: '古都金沢のおすすめ観光サイトのプラン', content: '金沢は古都であるとともに温泉や海鮮も楽しめる場所です。金沢空港や新幹線でのアクセスもよく...' },
     { title: '箱根の4月、5月の天気と服装', content: '箱根の天気について知り、天気の準備をしましょう。服装のご提案もいたします。...' },
     { title: '京都の4月、5月の天気と服装', content: '京都の天気について知り、天気の準備をしましょう。服装のご提案もいたします。...' },
-    { title: 'Pポイントでお得にお店探し', content: 'Pポイントを使えるお店を現地で探す' }
+    { title: '金沢のおすすめお食事どころ', content: 'レンタカーを借りて金沢を回るのはいかがでしょうか？車で回る場合のおすすめ食事どころをご提案します。' }
   ];
-  const colors = ["#4169e1", "#4169e1", "#dc143c"]
   return (
     <>
       <TextField
@@ -42,7 +41,7 @@ export default function ResultCards() {
         {cardData.map((card, index) => (
           <Grid item xs={12} sm={4} key={index}>
             {[0, 1, 2].includes(index) ? (
-            <Card sx={{ color: "#ffffff", backgroundColor: colors[index] }}>
+            <Card sx={{ color: "#ffffff", backgroundColor: "#00bfff" }}>
               <CardContent>
                 <Typography variant="subtitle1">{card.title}</Typography>
               </CardContent>
